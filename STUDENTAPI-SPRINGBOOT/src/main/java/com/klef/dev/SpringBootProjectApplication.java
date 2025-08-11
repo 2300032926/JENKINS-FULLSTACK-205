ddfe6c91f14a38fe559e15a83557093d4631c37e
Alekhya <2300032926cseh1@gmail.com>
2025-08-11 10:40:27 +0530

    Full stack project v1.0

--- STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/SpringBootProjectApplication.java
+++ STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/SpringBootProjectApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootProjectApplication extends SpringBootServletInitializer
-{
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProjectApplication.class, args);
-		System.out.println("Project is Running ...");
-	}
-	
+public class SpringBootProjectApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+        System.out.println("Project is Running ...");
+    }
 }
+
